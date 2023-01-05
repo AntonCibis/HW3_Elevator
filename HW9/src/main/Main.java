f8fa44161fb8c9ba1d163d1311943a2f8a034671
Anton Cibis <acibis@smail.uni-koeln.de>
2023-01-05 10:43:44 +0100

    test9

--- HW9/src/main/Main.java
+++ HW9/src/main/Main.java
@@ -2,6 +2,13 @@ package main;
 
 public class Main {
     /**
+     * not called
+     */
+    private Main() {
+
+    }
+
+    /**
      * This is test.
      */
     static final String TEST = "test";
